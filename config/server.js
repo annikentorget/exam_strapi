f0856b191293fd8045256d7541b0d87fb5e07ae1
Anniken Valstad Torget <annikentorget@gmail.com>
2021-05-05 19:54:17 +0200

    init project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7bee52cdbaa24ffc9bba1bb811ab182b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5ca9cd36-6732-445b-81f8-1e112e5b88c0'
+};
